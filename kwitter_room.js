a45f8a40018857ea24675513fb4953a87bade398
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2022-10-22 18:34:20 +0100

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,23 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyD2W7_SWyQupPiQ6ocim0C1__kuvs3sYb0",
+      authDomain: "kwitter-73d9e.firebaseapp.com",
+      databaseURL: "https://kwitter-73d9e-default-rtdb.firebaseio.com",
+      projectId: "kwitter-73d9e",
+      storageBucket: "kwitter-73d9e.appspot.com",
+      messagingSenderId: "933814068118",
+      appId: "1:933814068118:web:fb8ecc5ff96773513733b7",
+      measurementId: "G-SYD9P9NGX2"
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
